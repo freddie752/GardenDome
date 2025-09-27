6e0ec0ef595d69696b0a99c91ba9e04fcb8032dd
Freddie <freddieager@gmail.com>
2025-09-27 13:05:50 +0100

    Add test script for live feed

---
+++ src/live_feed.py
@@ -0,0 +1,13 @@
+from picamera2 import Picamera2
+import cv2
+
+picam2 = Picamera2()
+picam2.start()
+
+while True:
+    frame = picam2.capture_array()
+    cv2.imshow("Live Feed", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
