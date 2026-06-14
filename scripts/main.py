649b4c05141f321ca3600ae315ae0d23cef64c09
Freddie <freddieager@gmail.com>
2026-06-14 20:08:55 +0100

    Move non-working, multithread, live slackbot script.

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
-from detect_motion_record import MotionDetectorRecorder
-from slack import SlackBot
-from config import RECORDING_DIR, BITRATE, MOTION_THRESHOLD
+from scripts.detect_motion_record import MotionDetectorRecorder
+from logging.slack import SlackBot
+from config import RECORDING_DIR, BITRATE, MOTION_THRESHOLD, SLACK_CHANNEL
 import os
 import threading
 
@@ -8,9 +8,9 @@ SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 
 if __name__ == '__main__':
-    slack_bot = SlackBot()
+    slack_bot = SlackBot(SLACK_CHANNEL)
     motion_detector_recorder = MotionDetectorRecorder(RECORDING_DIR, BITRATE, MOTION_THRESHOLD, slack_bot=slack_bot)
-    slack_thread = threading.Thread(target=slack_bot.start, daemon=True)
+    slack_thread = threading.Thread(target=slack_bot.start_handler, daemon=True)
     motion_thread = threading.Thread(target=motion_detector_recorder.detect_motion_record, daemon=True)
 
     slack_thread.start()
