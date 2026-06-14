1439d775f612ada3964b33f2f62bdb4588129980
Freddie <freddieager@gmail.com>
2026-06-14 20:09:17 +0100

    Move config to root.

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 RECORDING_DIR = "data/recording/"    
 BITRATE = 2000000
 MOTION_THRESHOLD = 20  # Pixel intensity difference threshold to detect motion
+MOTION_FRACTION = 0.01  # Fraction of pixels that must change to trigger motion detection
 SLACK_LOGGING = True
 SLACK_CHANNEL = "C09M5FPTSLV"
