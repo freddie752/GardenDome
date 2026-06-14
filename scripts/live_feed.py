f2ac68244e91293883119b7ba369f2e268691b1c
Freddie <freddieager@gmail.com>
2026-06-14 20:06:52 +0100

    Move scripts to scripts dir.

--- scripts/live_feed.py
+++ scripts/live_feed.py
@@ -3,12 +3,15 @@ from picamera2 import Picamera2
 
 picam2 = Picamera2()
 picam2.start()
+picam2.rotation = 90
+
 cv2.namedWindow("Live Feed", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("Live Feed", 640, 480)
 
 while True:
     frame = picam2.capture_array()
-    cv2.imshow("Live Feed", frame)
+    rotated_frame = cv2.rotate(frame, cv2.ROTATE_180)
+    cv2.imshow("Live Feed", rotated_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
