1a71ddd5309bf1b82412a64af2cf98abc95b1ea2
freddie <freddieager@gmail.com>
2026-06-24 23:13:47 +0100

    Add conftest to mock picamera for dev outside of rasp pi.

---
+++ tests/camera/conftest.py
@@ -0,0 +1,6 @@
+from unittest.mock import MagicMock
+import sys
+
+sys.modules["picamera2"] = MagicMock()
+sys.modules["picamera2.outputs"] = MagicMock()
+sys.modules["picamera2.encoders"] = MagicMock()
